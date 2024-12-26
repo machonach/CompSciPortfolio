1fb2c44043b3b6db7b7b957b053be99aa3ca0c49
machonach <oghosh22009@gmail.com>
2024-12-26 22:12:48 +0000

    Made API and JSON file fetch system. Had to quit quote of the day thing since the API website wouldn't work. Put everything into folders and stylesheets for organization.

---
+++ javascript/main-javascript.js
@@ -0,0 +1,79 @@
+document.getElementById("hamburger").addEventListener("click", function(){
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("active");
+});
+
+// ABOUT ME JSON
+
+fetch('about-me.json')
+.then(response =>{
+    if (!response.ok){
+        throw new Error('Error fetching data');
+    }else{
+        return response.json();
+    };
+})
+
+.then(data =>{
+    const interestsList = document.getElementById("interests-list");
+    data.interests.forEach(interest =>{
+        const li = document.createElement("li");
+        li.textContent = interest;
+        interestsList.appendChild(li);
+    });
+
+    const languagesList = document.getElementById("coding-languages-list");
+    data.codingLanguages.forEach(codingLanguage =>{
+        const li = document.createElement("li");
+        li.textContent = codingLanguage;
+        languagesList.appendChild(li);
+    });
+        
+    document.getElementById('years-coding-value').textContent = data.yearsCoding;
+})
+
+.catch(error => {
+    console.error("Error fetching data");
+
+    const li = document.createElement("li");
+
+    const languagesList = document.getElementById("coding-languages-list");
+    li.textContent = "Failed to load coding languages";
+    languagesList.appendChild(li);
+
+    const interestsList = document.getElementById("interests-list");
+    li.textContent = "Failed to load interests";
+    interestsList.appendChild(li);
+});
+
+// JOKE OF THE MOMENT
+
+fetch("https://icanhazdadjoke.com/",{
+    method:'GET',
+    headers:{
+        'Accept':'application/json',
+        'User-Agent':'Portfolio Website CS Team Audition Project (https://github.com/machonach/CompSciPortfolio)'
+    }
+})
+
+.then(response =>{
+    if (!response.ok){
+        throw new Error('Error fetching data');
+    }else{
+        return response.json();
+    };
+})
+
+.then(data =>{
+    console.log(data)
+    const jokeText = document.getElementById("joke-text");
+
+    jokeText.textContent = data.joke;
+})
+
+.catch(error =>{
+    console.error(error)
+
+    const jokeText = document.getElementById("joke-text");
+    jokeText.textContent = "Failed to load joke.";
+});
---
+++ javascript/reaction-time-game-javascript.js
@@ -0,0 +1,72 @@
+function randomizeTarget(){
+    const targetStyle = document.getElementById("target").style;
+    const gameRect = document.getElementById("game").getBoundingClientRect();
+    const maxWidth = gameRect.width;
+    const maxHeight = gameRect.height;
+
+    const size = Math.floor((Math.random() * 8) + 8) * maxWidth / 100;
+    targetStyle.height = size + "px";
+    targetStyle.width = size + "px";
+
+    const maxX = maxWidth - size;
+    const maxY = maxHeight - size;
+    const xPos = Math.random() * maxX;
+    const yPos = Math.random() * maxY;
+    const scoreboardHeight = document.getElementById("scores").getBoundingClientRect().height
+
+    targetStyle.marginLeft = xPos + "px";
+    targetStyle.marginTop = (yPos + scoreboardHeight + 30) + "px";
+    
+    targetStyle.borderRadius = Math.floor(Math.random() * 51) + "%";
+    const colorList = ["#FF0000", "#FFA500", "#FFFF00", "#008000", "#0000FF", "#00FF00", "#800080"];
+    targetStyle.backgroundColor = colorList[Math.floor(Math.random() * colorList.length)];
+}
+
+function targetAppear() {
+    const targetStyle = document.getElementById("target").style;
+    targetStyle.display = "none";
+
+    setTimeout(() => {
+        randomizeTarget();
+        targetStyle.display = "block";
+        startTime = new Date().getTime();
+    }, Math.random() * 2000 + 500);
+}
+
+let bestTime = Infinity;
+let totalTime = 0;
+let amountOfHits = 0;
+let averageTime = 0;
+let startTime = null;
+let gameStarted = false;
+
+const targetStyle = document.getElementById("target").style;
+targetStyle.display = "none";
+setTimeout(Math.random() + 2000);
+targetStyle.display = "block";
+
+document.getElementById("target").addEventListener("click", () =>{
+    if (!gameStarted){
+        gameStarted = true;
+        target.innerText = "";
+        randomizeTarget();
+        startTime = new Date().getTime();
+    }else{
+        const endTime = new Date().getTime();
+        const reactionTime = endTime - startTime;
+
+        totalTime += reactionTime;
+        amountOfHits++;
+        averageTime = totalTime / amountOfHits;
+
+        if (reactionTime < bestTime) {
+            bestTime = reactionTime;
+        }
+
+        document.getElementById("time").innerHTML = ("Current reaction time: " + (reactionTime/1000) + " seconds");
+        document.getElementById("record-time").innerHTML = ("Personal record time: " + (bestTime/1000) + " seconds");
+        document.getElementById("average-time").innerHTML = ("Average reaction time: " + parseFloat((averageTime/1000).toFixed(3)) + " seconds");
+        
+        targetAppear();
+    }
+});
